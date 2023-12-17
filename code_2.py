26804e67378bd6142534cb58d800f7d9e3609036
MaxxKolah <140779767+MaxxKolah@users.noreply.github.com>
2023-12-18 00:44:50 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,59 +1,39 @@
 def generate_random_data():
-
-
-    data = generate_random_data()
-import random
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-    for item in data:
-def main():
-    main()
 if __name__ == "__main__":
-        print(f"Random Number: {item}")
-function main() {
-    });
-
-    data.forEach(item => {
-
-
-        console.log("Random Number: " + item);
-    for (let i = 0; i < 10; i++) {
-main();
-        data.push(Math.floor(Math.random() * 100) + 1);
-    const data = generateRandomData();
-    const data = [];
-function generateRandomData() {
-    return data;
-    }
-}
-}    data.forEach(item => {
-        data.push(Math.floor(Math.random() * 100) + 1);
-    }
-    for (let i = 0; i < 10; i++) {
-function main() {
-    return data;
-}
-function generateRandomData() {
-
-
-}
-
-    const data = [];
-    const data = generateRandomData();
-main();
-        console.log("Random Number: " + item);
-    });def generate_random_data():
-def main():
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-    return data
-if __name__ == "__main__":
-import random
-
 
     main()
-
-
+    data = generate_random_data()
         print(f"Random Number: {item}")
+
+
+import random
+def main():
+    for item in data:
+
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]if __name__ == "__main__":
+def main():
+
+    main()
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+    return data
+
+def generate_random_data():
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:def main():
+import random
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+def generate_random_data():
+
+    return data
+if __name__ == "__main__":
+    main()
+        print(f"Random Number: {item}")
+
